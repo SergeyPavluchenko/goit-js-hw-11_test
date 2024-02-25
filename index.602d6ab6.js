9d4b270308b74af4c737ea8834e28888cc001b8e
SergeyPavluchenko <102330212+SergeyPavluchenko@users.noreply.github.com>
2024-02-25 21:06:27 +0000

    Deploying to gh-pages from @ SergeyPavluchenko/goit-js-hw-11_test@4b74e7713eb0b197a191945edd1a1ae6109464f6 🚀

---
+++ index.602d6ab6.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("form");console.log(o);
+//# sourceMappingURL=index.602d6ab6.js.map
---
+++ index.703619bf.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("form");console.log(o)}();
+//# sourceMappingURL=index.703619bf.js.map
